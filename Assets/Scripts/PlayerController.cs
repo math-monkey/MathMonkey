9bdab41e0c99e69ac0368d6d3a27415c5d5af81d
Thais Lins <thaisfli@hotmail.com>
2018-05-13 02:09:40 -0300

    Add character movement and animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//movement variables
+	public float maxSpeed;
+
+	Rigidbody2D myRB;
+	Animator myAnim;
+	bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D>();
+		myAnim = GetComponent<Animator>();
+		facingRight = true;
+	}
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+		myAnim.SetFloat("speed", Mathf.Abs(move)); 
+
+		if(move>0 && !facingRight || move<0 && facingRight) {
+			Flip();
+		}
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
