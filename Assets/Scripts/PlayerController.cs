0759f67db815751adc4eb6e8dce167dd81471cf1
Thais Lins <thaisfli@hotmail.com>
2018-05-14 18:06:12 -0300

    Add character jump animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour {
 	//movement variables
 	public float maxSpeed;
 
+	//jumping variables
+	bool grounded = false;
+	float groundCheckRadius = 0.2f;
+	public LayerMask groundLayer;
+	public Transform groundCheck;
+	public float jumpHeight;
+
+
 	Rigidbody2D myRB;
 	Animator myAnim;
 	bool facingRight;
@@ -16,10 +24,24 @@ public class PlayerController : MonoBehaviour {
 		myRB = GetComponent<Rigidbody2D>();
 		myAnim = GetComponent<Animator>();
 		facingRight = true;
+		grounded = true;
 	}
+
+	// Update is called once per frame
+    void Update () {
+		if(grounded && Input.GetAxis("Jump") > 0) {
+			grounded = false;
+			myAnim.SetBool("isGrounded", grounded);
+			myRB.AddForce(new Vector2(0, jumpHeight));
+		}
+    }
     
-    // Update is called once per frame
     void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+		myAnim.SetBool("isGrounded", grounded);
+
+		myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+
 		float move = Input.GetAxis("Horizontal");
         myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
 		myAnim.SetFloat("speed", Mathf.Abs(move)); 
