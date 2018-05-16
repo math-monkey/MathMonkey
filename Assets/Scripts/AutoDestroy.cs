fe1a91093ca0c249bbf7d9f3a55757b465e98bf0
Elton Viana <eltonvs@outlook.com>
2018-05-16 03:29:43 -0300

    Start projectile implementation

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+
+	public float aliveTime;
+
+	// Use this for initialization
+	void Awake() {
+		Destroy(gameObject, aliveTime);
+	}
+
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+	public float degsBySec;
+	public float projectileSpeed;
+
+	Rigidbody2D rb;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+		rb.AddForce(new Vector2(1, 0) * projectileSpeed, ForceMode2D.Impulse);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.Rotate(0, 0, -degsBySec * Time.deltaTime);
+	}
+}
