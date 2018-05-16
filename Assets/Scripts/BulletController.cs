f0f68334194170963ba9bbf6292555d264c29a9d
Elton Viana <eltonvs@outlook.com>
2018-05-16 15:37:43 -0300

    Add explosion when bullet hits a Shootable layer

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileController : MonoBehaviour {
+public class BulletController : MonoBehaviour {
 
 	public float degsBySec;
-	public float projectileSpeed;
+	public float bulletSpeed;
 
 	Rigidbody2D rb;
 
 	void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 		var direction = transform.localRotation.z.Equals(0) ? Vector2.right : Vector2.left;
-		rb.AddForce(direction * projectileSpeed, ForceMode2D.Impulse);
+		rb.AddForce(direction * bulletSpeed, ForceMode2D.Impulse);
 	}
 
 	// Update is called once per frame
 	void Update() {
 		transform.Rotate(0, 0, -degsBySec * Time.deltaTime);
+	}
+
+	public void RemoveForce() {
+		rb.velocity = Vector2.zero;
 	}
 }
---
+++ Assets/Scripts/BulletHit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit : MonoBehaviour {
+
+	public float bulletDamage;
+	public GameObject explosionEffect;
+
+	BulletController bc;
+
+	void Awake() {
+		bc = GetComponentInParent<BulletController>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		HitShootable(other);
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		HitShootable(other);
+	}
+
+	void HitShootable(Collider2D other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("Shootable")) {
+			bc.RemoveForce();
+			Instantiate(explosionEffect, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
