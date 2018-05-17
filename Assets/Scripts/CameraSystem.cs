0c059b3f6ee709c57b1bbc8b8453915e37218b32
Thais Lins <thaisfli@hotmail.com>
2018-05-17 00:30:26 -0300

    Add options menu

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -20,7 +20,7 @@ public class CameraSystem : MonoBehaviour {
 	void FixedUpdate () {
 		Vector3 targetCamPos = target.position + offset;
 
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing*Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
 
 		if(transform.position.y < lowY) {
 			transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,24 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+    
+	public GameObject mainMenuObj;
+	public GameObject optionsMenuObj;
 
 	public void PlayGame() {
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 	}
 
 	public void QuitGame() {
 		Application.Quit();
 		Debug.Log("Quit");
+	}
+    
+	public void OptionsMenu() {
+		SceneManager.LoadScene("OptionsMenu");
+	}
+
+	public void Back() {
+		SceneManager.LoadScene("MainMenu");
 	}
 }
