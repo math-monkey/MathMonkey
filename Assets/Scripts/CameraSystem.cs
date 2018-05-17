987331f9aac748d368841cbba5db27056eb916ab
Elton Viana <eltonvs@outlook.com>
2018-05-17 10:47:33 -0300

    Fix bullet explosion and autoformat CameraSystem code

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
 
-	public Transform target; //what camera is following
-	public float smoothing;
+    public Transform target; // What camera is following
+    public float smoothing;
 
-	Vector3 offset;
-	float lowY;
-	float lowX;
+    Vector3 offset;
+    float lowY;
+    float lowX;
 
-	// Use this for initialization
-	void Start () {
-		offset = transform.position;
-		lowY = transform.position.y;
-		lowX = transform.position.x;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Vector3 targetCamPos = target.position + offset;
+    // Use this for initialization
+    void Start() {
+        offset = transform.position;
+        lowY = transform.position.y;
+        lowX = transform.position.x;
+    }
 
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing*Time.deltaTime);
-
-		if(transform.position.x < lowX) {
-			transform.position = new Vector3(lowX, transform.position.y, transform.position.z);
-		}
-
-		if(transform.position.y < lowY) {
-			transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
-		}
-	}
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 targetCamPos = target.position + offset;
+        var nextPos = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Max(nextPos.x, lowX), Mathf.Max(nextPos.y, lowY), nextPos.z);
+    }
 }
