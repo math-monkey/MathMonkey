cc3716f3fb0cfb2f6ee70d7669e1e6b3e84f5024
Thais Lins <thaisfli@hotmail.com>
2018-05-17 02:01:53 -0300

    Modifications on Camera System

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -9,18 +9,24 @@ public class CameraSystem : MonoBehaviour {
 
 	Vector3 offset;
 	float lowY;
+	float lowX;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position;
 		lowY = transform.position.y;
+		lowX = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector3 targetCamPos = target.position + offset;
 
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing*Time.deltaTime);
+
+		if(transform.position.x < lowX) {
+			transform.position = new Vector3(lowX, transform.position.y, transform.position.z);
+		}
 
 		if(transform.position.y < lowY) {
 			transform.position = new Vector3(transform.position.x, lowY, transform.position.z);
