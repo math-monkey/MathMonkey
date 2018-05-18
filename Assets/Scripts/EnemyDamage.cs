6dbc73fc67d49b991aa2cc0c71215282263e3b4f
Thais Lins <thaisfli@hotmail.com>
2018-05-18 04:34:31 -0300

    Add enemy damage and player health

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+	public float damage;
+	public float damageRate;
+	public float pushBackForce;
+    
+	float nextDamage;
+
+	// Use this for initialization
+	void Start () {
+		nextDamage = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if(other.tag == "Player" && nextDamage < Time.time) {
+			PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+			playerHealth.AddDamage(damage);
+			nextDamage = Time.time + damageRate;
+
+			PushBack(other.transform);
+		}
+	} 
+
+	void PushBack(Transform pushedObject) {
+		Vector2 pushDirection = new Vector2(0,pushedObject.position.y - transform.position.y).normalized;
+		pushDirection *= pushBackForce;
+		Rigidbody2D pushRB = pushedObject.gameObject.GetComponent<Rigidbody2D>();
+		pushRB.velocity = Vector2.zero;
+		pushRB.AddForce(pushDirection, ForceMode2D.Impulse); 
+	}
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
 
+	public float fullHealth;
+	float currentHealth;
+    
+	PlayerController controlMovement;
+	Transform enemyObject;
+	Animator myAnim;
+
 	// Use this for initialization
 	void Start () {
-		
+		currentHealth = fullHealth;
+		controlMovement = GetComponent<PlayerController>();
+		myAnim = controlMovement.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+    //adds damage to player
+	public void AddDamage(float damage) {
+		if (damage <= 0) return;
+		currentHealth -= damage;
+
+		if(currentHealth <= 0) {
+			MakeDead();
+		}
+	}
+
+	void MakeDead() {
+        myAnim.SetTrigger("die");
+		Destroy(gameObject, 0.6f);
 	}
 }
