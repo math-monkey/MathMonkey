fd259b5b62e255161b9479a0ef633ba2a6a36e7e
Elton Viana <eltonvs@outlook.com>
2018-05-19 18:22:58 -0300

    Add damage indicator to enemy (and explosion)

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
 
+	public GameObject enemyDeathFX;
+	public Slider enemySlider;
 	public float enemyMaxHealth;
 	float currentHealth;
 
 	// Use this for initialization
 	void Start() {
 		currentHealth = enemyMaxHealth;
+		enemySlider.maxValue = enemyMaxHealth;
+		enemySlider.value = currentHealth;
 	}
 
 	public void AddDamage(float damage) {
 		currentHealth -= damage;
+		enemySlider.gameObject.SetActive(true);
+		enemySlider.value = currentHealth;
 		if (currentHealth <= 0) {
 			MakeDead();
 		}
@@ -21,5 +28,6 @@ public class EnemyHealth : MonoBehaviour {
 
 	private void MakeDead() {
 		Destroy(gameObject);
+		Instantiate(enemyDeathFX, transform.position, transform.rotation);
 	}
 }
