c6ee9ea87431bc95297f26c7d62afdd1ffe2200a
Thais Lins <thaisfli@hotmail.com>
2018-05-19 00:57:22 -0300

    Disable player controls and collider on death

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,22 +6,26 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour {
 
 	public float fullHealth;
+
+	bool isDead;
 	float currentHealth;
-    
 	PlayerController controlMovement;
 	Transform enemyObject;
 	Animator myAnim;
-
+	CapsuleCollider2D capsuleCollider;
+    
 	// Use this for initialization
 	void Start () {
+		isDead = false;
 		currentHealth = fullHealth;
 		controlMovement = GetComponent<PlayerController>();
 		myAnim = controlMovement.GetComponent<Animator>();
+		capsuleCollider = GetComponent<CapsuleCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
     //adds damage to player
@@ -33,9 +37,11 @@ public class PlayerHealth : MonoBehaviour {
 			MakeDead();
 		}
 	}
-
+   
 	void MakeDead() {
-        myAnim.SetTrigger("die");
-		Destroy(gameObject, 0.6f);
+		capsuleCollider.enabled = false;
+		controlMovement.enabled = false;
+        myAnim.SetTrigger("Die");
+		Destroy(gameObject, 3f);
 	}
 }
