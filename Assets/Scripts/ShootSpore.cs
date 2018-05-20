48b6ac24ee2b22d2fae245c2cda7aa2659965759
Thais Lins <thaisfli@hotmail.com>
2018-05-20 01:55:23 -0300

    Enemy shoots spores

---
+++ Assets/Scripts/ShootSpore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class ShootSpore : MonoBehaviour {
+
+    public GameObject theProjectile;
+    public float shootTime;
+    public int chanceShoot;
+    public Transform shootFrom;
+
+    float nextShootTime;
+
+    // Use this for initialization
+    void Start () {
+        nextShootTime = 0f;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+	void OnTriggerStay2D(Collider2D other) {
+        if(other.tag == "Player" && nextShootTime < Time.time) {
+            nextShootTime = Time.time + shootTime;
+            if(Random.Range(0,10) >= chanceShoot) {
+                Instantiate(theProjectile, shootFrom.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SporeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporeController : MonoBehaviour {
+
+    public float sporeSpeedHigh;
+    public float sporeSpeedLow;
+    public float sporeAngle;
+    public float sporeTorqueAngle;
+
+    Rigidbody2D sporeRB;
+
+    // Use this for initialization
+    void Start() {
+        sporeRB = GetComponent<Rigidbody2D>();
+        Vector2 pushDirection = new Vector2(Random.Range(-sporeAngle, sporeAngle), Random.Range(sporeSpeedLow, sporeSpeedHigh));
+        sporeRB.AddForce(pushDirection, ForceMode2D.Impulse);
+        sporeRB.AddTorque(Random.Range(-sporeTorqueAngle, sporeTorqueAngle));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
