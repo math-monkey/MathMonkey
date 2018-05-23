3b1f8f7505ca40b4f35de8b78b14cdc429d913ac
Elton Viana <eltonvs@outlook.com>
2018-05-23 16:16:09 -0300

    Add How to Play screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,25 +6,32 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-    public GameObject mainMenuObj;
-    public GameObject optionsMenuObj;
+	public GameObject mainMenuObj;
+	public GameObject optionsMenuObj;
+	public GameObject instructionsMenuObj;
 
 	public void PlayGame() {
 		SceneManager.LoadScene("Main");
 	}
 
-    public void QuitGame() {
-        Application.Quit();
-        Debug.Log("Quit");
-    }
-    
-    public void OptionsMenu() {
-        mainMenuObj.SetActive(false);
-        optionsMenuObj.SetActive(true);
-    }
+	public void QuitGame() {
+		Application.Quit();
+		Debug.Log("Quit");
+	}
 
-    public void Back() {
-        optionsMenuObj.SetActive(false);
-        mainMenuObj.SetActive(true);
-    }
+	public void OptionsMenu() {
+		mainMenuObj.SetActive(false);
+		optionsMenuObj.SetActive(true);
+	}
+
+	public void InstructionsMenu() {
+		mainMenuObj.SetActive(false);
+		instructionsMenuObj.SetActive(true);
+	}
+
+	public void Back() {
+		optionsMenuObj.SetActive(false);
+		instructionsMenuObj.SetActive(false);
+		mainMenuObj.SetActive(true);
+	}
 }
