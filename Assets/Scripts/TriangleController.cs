25b6e50eae107347ed67179422c708d9cfc1cd4c
Thais Lins <thaislin@gmail.com>
2018-11-29 03:19:33 -0300

    Randomize type of triangle to be displayed

--- Assets/Scripts/TriangleController.cs
+++ Assets/Scripts/TriangleController.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class TriangleController : MonoBehaviour {
 
     public GameObject triangleObject;
-    int triangleType;
+    MeshGenerator meshGenerator;
+    int enemyTriangle;
 
     void Start() {
         UpdateTriangle();
     }
 
     public int GetNumber() {
-        return triangleType;
+        return enemyTriangle;
     }
 
     public void UpdateTriangle() {
         int idx = Random.Range(0, TriangleHelper.availableTriangles.Length);
-        //enemyTriangle = TriangleHelper.availableTriangles[idx];
-        //MeshGenerator meshGenerator = new MeshGenerator(2, 1)
-        MeshGenerator meshGenerator = triangleObject.GetComponent<MeshGenerator>();
-        meshGenerator.SetValues(2, 1);
-        //enemyNumberUI.text = enemyNumber.ToString();
+        enemyTriangle = TriangleHelper.availableTriangles[idx];
+        meshGenerator = triangleObject.GetComponent<MeshGenerator>();
+        SetTriangle();
+    }
+
+    public void SetTriangle() {
+        switch (enemyTriangle) {
+            case 1:
+                meshGenerator.SetValues(2, 1); //Equilateral Triangle
+                break;
+            case 2:
+                meshGenerator.SetValues(4, 1); //Isosceles Triangle
+                break;
+            case 3:
+                meshGenerator.SetValues(2, 3); //Scalene Triangle
+                break;
+            default:
+                break;
+        }
     }
 }
